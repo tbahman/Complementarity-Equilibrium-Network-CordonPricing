e003257c1a4d15a218a6d5e81bf21ba2ec3a2f2d
tbahman <66690650+tbahman@users.noreply.github.com>
2024-10-11 13:39:32 -0500

    Update algorithm.h

--- algorithm.h
+++ algorithm.h
@@ -9,3 +9,9 @@ void BELL(double *DIST, int NORG, double D[], int *ARCB, int *LEVEL, int *PN, in
 void CycleCost(int nCYCLE, const double *fa1, const double *ta1, const double *fa2, const double *ta2, CYCLEINFOSTRUCT CYCLEinfo[]);
 double PathOdError(PATHPOINTER PathStart, int npath, double fa[], double Tbell, double demand, bool);
 double RELGAP(PATHPOINTER PathStart, double fa[], double Tbell, double demand, bool);
+void INITIALVALUE(PATHPOINTER *& PATHstart, double *& DEMAND, double*& fa, double *& ta, double TP[], CYCLEINFOSTRUCT CYCLEinfo[], PRGPATHINFOSTRC *&PRGPTHinfo);
+void STARTPOINT(double *DEMAND, PATHPOINTER *PATHstart, double fa[], double *ta, PRGMEMINFOSTRC *PRGMEMinfo, PRGPATHINFOSTRC *PRGPTHinfo, bool classno);
+int UPDATE_PATHS(PATHPOINTER *PATHstart, int od, double TP[], double fa[], double *Tmax, double *Tmin, double Tbell, int ARCB[], int NODB[], int numofArcs, PRGMEMINFOSTRC *PRGMEMinfo, PRGPATHINFOSTRC *PRGPTHinfo, double nRG, bool classno);
+void LINEARIZE(int npath1, int npath2, double demand, PATHPOINTER PathStart1, PATHPOINTER PathStart2, double fa[], double TP[], double AM[][ MAXNUMPATH+1 ], double Q[], int od, double demandOne, double demandTwo);
+void UPDATE_hpfaTPErr(int npath1, int npath2, double Z1[], double Z2[], PATHPOINTER PathStart1, PATHPOINTER PathStart2, double fa1[], double fa2[], double TP1[], double TP2[], double *Err1, double *Err2, double& Tmin1, double& Tmin2);
+inline double CALCta(double *faFormal, int I, bool classno);
