4cbc272a5bc53cd80e8b32a5252f5f0cb7fd4bf4
tbahman <66690650+tbahman@users.noreply.github.com>
2024-10-16 17:06:59 -0500

    Update algorithm.cpp: INITIA

--- algorithm.cpp
+++ algorithm.cpp
@@ -31,4 +31,36 @@ void LCP(double AM[][ MAXNUMPATH+1 ], double Q[], int N, double Z[])
             }
         }
     } /* MATRIX */
-
+   /* INITIA (N); ***************************************** */
+    {
+        int I = 1, J = 2, L;
+        double T1;
+        for (J = 2; J <= N; ++J) {
+            if (Q[I] > Q[J]) {
+                I = J;
+            }
+        }
+        IR = I;
+        T1 = -Q[IR];
+        if (T1 <= 0.0)
+            return;
+        for (I = 1; I <= N; ++I) {
+            Q[I] = Q[I] + T1;
+        }
+        Q[IR] = T1;
+        for (J = 1; J <= N; ++J) {
+            BB[J][IR] = -1.0;
+            W[J] = Q[J];
+            Z[J] = 0.0;
+            MBASIS[J] = 1;
+            L = N + J;
+            MBASIS[L] = J;
+        }
+        NL1 = 1;
+        L = N + IR;
+        NL2 = IR;
+        MBASIS[IR] = 3;
+        MBASIS[L] = 0;
+        W[IR] = 0.0;
+        L1 = 1;
+    } /* INITIA */
