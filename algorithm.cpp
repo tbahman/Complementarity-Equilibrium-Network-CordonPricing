9cb9607529c544715ef69a62d4e2b54ea6cc6e52
tbahman <66690650+tbahman@users.noreply.github.com>
2024-10-16 14:21:54 -0500

    Update algorithm.cpp:   MATRIX

--- algorithm.cpp
+++ algorithm.cpp
@@ -8,3 +8,27 @@ void UPDATEta(double fa[], double *ta, bool classno)
     for (int I = 0; I < NARCS; ++I)
         ta[I] = CALCta(fa, I, classno);
 }
+
+void LCP(double AM[][ MAXNUMPATH+1 ], double Q[], int N, double Z[])
+{
+    int IR, L1, NL1, NL2, NE1, NE2;
+    int *MBASIS;
+
+    double BB[MAXNUMPATH+2][MAXNUMPATH+2], AA[MAXNUMPATH+2], W[MAXNUMPATH+2];
+
+    if (!(MBASIS = (int *)calloc(MBASISNUM, sizeof(int))))
+        ErrorAlloc(" MBASIS in LCP ");
+
+    /*  MATRIX (N); ***************************************** */
+    {
+        int I, J;
+        for (J = 1; J <= N; ++J) {
+            for (I = 1; I <= N; ++I) {
+                if (I == J)
+                    BB[I][J] = 1;
+                else
+                    BB[I][J] = 0.0;
+            }
+        }
+    } /* MATRIX */
+
