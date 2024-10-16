fc3b5440b743d91173ad13eef12a98b58b74c77d
tbahman <66690650+tbahman@users.noreply.github.com>
2024-10-16 09:47:39 -0500

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,1 +1,10 @@
+// algorithm.cpp
+#include "algorithm.h"
 
+// Function definitions
+
+void UPDATEta(double fa[], double *ta, bool classno)
+{
+    for (int I = 0; I < NARCS; ++I)
+        ta[I] = CALCta(fa, I, classno);
+}
